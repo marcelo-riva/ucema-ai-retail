521abfaae31b95cfb4a635cab604f0253efa05da
Marcelo Riva <marceloriva.sys@gmail.com>
2026-06-26 17:57:59 -0300

    fix: resuelve import TS de Amplify backend

--- laboratorio_v3/apps/web/amplify/backend.ts
+++ laboratorio_v3/apps/web/amplify/backend.ts
@@ -1,5 +1,5 @@
 import { defineBackend } from "@aws-amplify/backend";
-import { data } from "./data/resource.js";
+import { data } from "./data/resource.ts";
 
 /**
  * Amplify Gen 2 backend definition for NEXUS Retail Labs.
